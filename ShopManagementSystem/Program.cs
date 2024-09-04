9d9d0d2129a8ddc0dccb22e0873c16aca88ba8a3
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-09-04 14:17:15 +0630

    feat: add services

--- ShopManagementSystem/Program.cs
+++ ShopManagementSystem/Program.cs
@@ -1,8 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using ShopManagementSystem.Database.Models;
+using ShopManagementSystem.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection")));
 
-builder.Services.AddControllers();
+builder.Services.AddScoped<ShopService>();
+
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.Preserve;
+    options.JsonSerializerOptions.MaxDepth = 64;  // You can increase the max depth if needed
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
