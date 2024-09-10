b9eac67707f085ac09109487f00111450bb1bdda
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-09-10 23:36:35 +0630

    feat: add location

---
+++ ShopManagementSystem.Backend/Features/LocationEndpoints.cs
@@ -0,0 +1,57 @@
+﻿using ShopManagementSystem.Database.Models;
+
+namespace ShopManagementSystem.Backend.Features
+{
+    public static class LocationEndpoints
+    {
+        public static void AddLocationEndpoint(this WebApplication app)
+        {
+            // Get all locations
+            app.MapGet("/locations", async (LocationRepository repository) =>
+            {
+                var locations = await repository.GetLocationsAsync();
+                return Results.Ok(locations);
+            });
+
+            // Get location by Id
+            app.MapGet("/locations/{id}", async (int id, LocationRepository repository) =>
+            {
+                var location = await repository.GetLocationByIdAsync(id);
+                return location is not null ? Results.Ok(location) : Results.NotFound();
+            });
+
+            // Create a new location
+            app.MapPost("/locations", async (Location location, LocationRepository repository) =>
+            {
+                await repository.CreateLocationAsync(location);
+                return Results.Created($"/locations/{location.Id}", location);
+            });
+
+            // Update a location
+            app.MapPut("/locations/{id}", async (int id, Location location, LocationRepository repository) =>
+            {
+                var existingLocation = await repository.GetLocationByIdAsync(id);
+                if (existingLocation is null)
+                {
+                    return Results.NotFound();
+                }
+
+                await repository.UpdateLocationAsync(id, location);
+                return Results.NoContent();
+            });
+
+            // Delete a location
+            app.MapDelete("/locations/{id}", async (int id, LocationRepository repository) =>
+            {
+                var location = await repository.GetLocationByIdAsync(id);
+                if (location is null)
+                {
+                    return Results.NotFound();
+                }
+
+                await repository.DeleteLocationAsync(id);
+                return Results.NoContent();
+            });
+        }
+    }
+}
---
+++ ShopManagementSystem.Backend/Features/LocationRepository.cs
@@ -0,0 +1,71 @@
+﻿using Dapper;
+using ShopManagementSystem.Backend.Services;
+using ShopManagementSystem.Database.Models;
+
+namespace ShopManagementSystem.Backend.Features;
+
+public class LocationRepository
+{
+    private readonly DapperContext _context;
+
+    public LocationRepository(DapperContext context)
+    {
+        _context = context;
+    }
+
+    // Get all locations
+    public async Task<IEnumerable<Location>> GetLocationsAsync()
+    {
+        var query = "SELECT * FROM Location";
+
+        using (var connection = _context.CreateConnection())
+        {
+            var locations = await connection.QueryAsync<Location>(query);
+            return locations.ToList();
+        }
+    }
+
+    // Get location by Id
+    public async Task<Location> GetLocationByIdAsync(int id)
+    {
+        var query = "SELECT * FROM Location WHERE Id = @Id";
+
+        using (var connection = _context.CreateConnection())
+        {
+            return await connection.QuerySingleOrDefaultAsync<Location>(query, new { Id = id });
+        }
+    }
+
+    // Create new location
+    public async Task CreateLocationAsync(Location location)
+    {
+        var query = "INSERT INTO Location (Name) VALUES (@Name)";
+
+        using (var connection = _context.CreateConnection())
+        {
+            await connection.ExecuteAsync(query, new { location.Name });
+        }
+    }
+
+    // Update a location
+    public async Task UpdateLocationAsync(int id, Location location)
+    {
+        var query = "UPDATE Location SET Name = @Name WHERE Id = @Id";
+
+        using (var connection = _context.CreateConnection())
+        {
+            await connection.ExecuteAsync(query, new { location.Name, Id = id });
+        }
+    }
+
+    // Delete a location
+    public async Task DeleteLocationAsync(int id)
+    {
+        var query = "DELETE FROM Location WHERE Id = @Id";
+
+        using (var connection = _context.CreateConnection())
+        {
+            await connection.ExecuteAsync(query, new { Id = id });
+        }
+    }
+}
