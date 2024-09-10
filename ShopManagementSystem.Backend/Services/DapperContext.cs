21c2ac716ba08c9509bbaf0fd67e2b8d89574688
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-09-10 22:53:02 +0630

    feat: add dapper context

---
+++ ShopManagementSystem.Backend/Services/DapperContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace ShopManagementSystem.Backend.Services;
+
+public class DapperContext
+{
+    private readonly IConfiguration _configuration;
+    private readonly string _connectionString;
+
+    public DapperContext(IConfiguration configuration)
+    {
+        _configuration = configuration;
+        _connectionString = _configuration.GetConnectionString("DbConnection")!;
+    }
+
+    public IDbConnection CreateConnection() => new SqlConnection(_connectionString);
+}
